421217e8a5bdd62ee6a28310bdb8e1bff6d55b5e
Lasta Apps <krasik.peta@seznam.cz>
2023-09-02 19:02:55 +0300

    feat: Config reading

---
+++ src/config.rs
@@ -0,0 +1,60 @@
+use chrono::NaiveDate;
+use config_file::FromConfigFile;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ConfigDto {
+    cal_num: u32,
+    from: String,
+    to: String,
+}
+
+impl ConfigDto {
+    fn to_domain(&self) -> Config {
+        Config {
+            cal_num: self.cal_num,
+            from: ConfigDto::parse_date(&self.from),
+            to: ConfigDto::parse_date(&self.to),
+        }
+    }
+
+    fn parse_date(date: &str) -> u64 {
+        let date = NaiveDate::parse_from_str(date, "%Y-%m-%d").unwrap();
+        date.and_hms_milli_opt(0, 0, 0, 0)
+            .unwrap()
+            .timestamp_millis() as u64
+    }
+}
+
+pub struct Config {
+    pub cal_num: u32,
+    pub from: u64,
+    pub to: u64,
+}
+
+pub fn load_config() -> Config {
+    let directories = ["hopitude.toml", "~/.config/hopitude.toml"];
+
+    let mut cfg_dto: Option<ConfigDto> = None;
+
+    for dir in directories {
+        match ConfigDto::from_config_file(dir) {
+            Ok(cfg) => {
+                cfg_dto = Some(cfg);
+                break;
+            }
+            Err(e) => match &e {
+                config_file::ConfigFileError::FileAccess(_) => continue,
+                config_file::ConfigFileError::Toml(toml) => panic!("{}, {}", e, toml),
+                config_file::ConfigFileError::UnsupportedFormat => panic!("{}", e),
+            },
+        };
+    }
+    let cfg = if let Some(cfg) = cfg_dto {
+        cfg
+    } else {
+        panic!("No config file found!");
+    };
+
+    cfg.to_domain()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = config::load_config();
+
+    let base_url = format!("https://admin.hopitude.com/api/v1/calendar/workout-events/club/{}/?from={}&to={}", config.cal_num, config.from, config.to);
+
+    println!("{}", base_url);
 }
+
